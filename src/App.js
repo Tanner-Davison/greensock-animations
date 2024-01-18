3ebfb5f7175b970e268d89ebe72c59d29b198b97
Tanner Davison <tanner.davison95@gmail.com>
2024-01-17 19:22:52 -0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import styled from "styled-components";
+import { gsap } from "gsap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  useEffect(() => {
+    gsap.to(
+      "#headline",
+      {
+        x: -200,duration: 1, repeat:-1, yoyo:true,
+      });
+    gsap.fromTo("#headline2",{
+      x:200, duration:1 , repeat:-1, yoyo:true,
+    })
+  
+    }, []);
+    
+    return (
+    <Wrapper>
+      <Headline id="headline">Bouncy </Headline>
+      <Headline id="headline2">Balls</Headline>
+     
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Headline = styled.h2`
+  color: black;
+  font-family: "Archivo", sans-serif;
+  border: 2px solid gray;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+`;
