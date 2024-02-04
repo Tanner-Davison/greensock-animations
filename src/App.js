eff7547ca1199547d0e5c69d01c1edd896b35d1a
Tanner Davison <Tboydavison@gmail.com>
2024-02-04 02:18:59 -0700

    deleted scrolltoDemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import CustomCarousel from './components/CustomCarousel'
 import LogosContainer from './components/LogosContainer'
-import ScrollToDemo from './components/ScrollToDemo'
 
 const scrollToSection = (sectionId) => {
   const element = document.getElementById(sectionId)
@@ -16,7 +15,6 @@ function App() {
   return (
     <>
       <CustomCarousel scrollto={scrollToSection} />
-      <ScrollToDemo scrollto={scrollToSection} />
       <LogosContainer scrollto={scrollToSection} />
     </>
   )
